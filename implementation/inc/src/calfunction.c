2250a257d41b38baccbd4519a304769c7c7f42a9
mssalmansk <47742185+mssalmansk@users.noreply.github.com>
2021-09-08 16:28:03 +0530

    Update calfunction.c

--- implementation/inc/src/calfunction.c
+++ implementation/inc/src/calfunction.c
@@ -8,58 +8,10 @@
  * @copyright Copyright (c) 2021
  * 
  */
-int addition();  
-int subtract();  
-int multiply();  
-int divide();     
-void exit();  
-   
-int main()  
-{    
-    int oprt;  
-    do  
-    {    
-        printf (" Select an  all operation to perform the calculation in C Calculator: ");  
-        printf (" \n 1 Addition  \t \t 2 Subtraction \n 3 Multiplication \t 4 Division  \n 5 Exit \n \n Please, Make a choice ");      
-          
-        scanf ("%d", &oprt);  
-        
-    switch (oprt)  
-    {  
-        case 1:  
-            addition(); 
-            break; 
-              
-        case 2:  
-            subtract(); 
-            break;
-              
-        case 3:  
-            multiply();  
-            break; 
-              
-        case 4:  
-            divide();  
-            break;
-              
-        case 5:  
-            exit(0);   
-            break;
-              
-        default:  
-            printf(" Something is wrong!! ");  
-            break;                        
-    }  
-    printf (" \n \n ********************** \n ");  
-    } while (oprt != 7);  
-      
-  
-    return 0;        
-}  
-  
-  
-  
- 
+#include <stdio.h>  
+#include <conio.h>  
+#include <math.h>  
+#include <stdlib.h>
 int addition()  
 {  
     int i, sum = 0, num, f_num;    
@@ -74,32 +26,3 @@ int addition()
     printf (" Total Sum of the numbers = %d", sum);  
     return 0;  
 }  
-    
-int subtract()  
-{  
-    int n1, n2, result;  
-    printf (" The first number is: ");  
-    scanf ("  %d", &n1);  
-    printf (" The second number is: ");  
-    scanf ("  %d", &n2);  
-    result = n1 - n2;    
-    printf (" The subtraction of %d - %d is: %d", n1, n2, result);  
-}  
-    
-int multiply()  
-{  
-    int n1, n2, result;  
-    printf (" The first number is: ");  
-    scanf ("  %d", &n1);  
-    printf (" The second number is: ");  
-    scanf ("  %d", &n2);  
-    result = n1 * n2;    
-    printf (" The multiply of %d * %d is: %d", n1, n2, result);  
-}    
-int divide()  
-{  
-    int n1, n2, result;  
-    printf (" The first number is: ");  
-    scanf ("  %d", &n1);  
-    printf (" The second number is: ");  
-    scanf ("  %d", &n2);  
