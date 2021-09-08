6b3e817d1f4ed7c0eef57301f3591fed5ddc749c
mssalmansk <47742185+mssalmansk@users.noreply.github.com>
2021-09-08 14:19:44 +0530

    Update calfunction.c

--- implementation/inc/src/calfunction.c
+++ implementation/inc/src/calfunction.c
@@ -1,1 +1,115 @@
-
+/**
+ * @file calfuntion.c
+ * @author md salman
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+int addition();  
+int subtract();  
+int multiply();  
+int divide();   
+int sqrt1();
+int sq();  
+void exit();  
+   
+int main()  
+{    
+    int oprt;  
+    do  
+    {    
+        printf (" Select an  all operation to perform the calculation in C Calculator: ");  
+        printf (" \n 1 Addition  \t \t 2 Subtraction \n 3 Multiplication \t 4 Division \n 5 Square \t \t 6 Square Root \n 7 Exit \n \n Please, Make a choice ");      
+          
+        scanf ("%d", &oprt);  
+        
+    switch (oprt)  
+    {  
+        case 1:  
+            addition(); 
+            break; 
+              
+        case 2:  
+            subtract(); 
+            break;
+              
+        case 3:  
+            multiply();  
+            break; 
+              
+        case 4:  
+            divide();  
+            break; 
+              
+        case 5:  
+            sq();   
+            break; 
+              
+        case 6:  
+            sqrt1(); 
+            break; 
+              
+        case 7:  
+            exit(0);   
+            break;
+              
+        default:  
+            printf(" Something is wrong!! ");  
+            break;                        
+    }  
+    printf (" \n \n ********************** \n ");  
+    } while (oprt != 7);  
+      
+  
+    return 0;        
+}  
+  
+  
+  
+ 
+int addition()  
+{  
+    int i, sum = 0, num, f_num;    
+    printf (" How many numbers you want to add: ");  
+    scanf ("%d", &num);  
+    printf (" Enter the numbers: \n ");  
+    for (i = 1; i <= num; i++)  
+    {  
+        scanf(" %d", &f_num);  
+        sum = sum + f_num;  
+    }  
+    printf (" Total Sum of the numbers = %d", sum);  
+    return 0;  
+}  
+    
+int subtract()  
+{  
+    int n1, n2, result;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
+    result = n1 - n2;    
+    printf (" The subtraction of %d - %d is: %d", n1, n2, result);  
+}  
+    
+int multiply()  
+{  
+    int n1, n2, result;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
+    result = n1 * n2;    
+    printf (" The multiply of %d * %d is: %d", n1, n2, result);  
+}    
+int divide()  
+{  
+    int n1, n2, result;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
