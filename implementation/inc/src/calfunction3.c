005608f195f980dd294a163c49b58b99b49a1dcf
mssalmansk <47742185+mssalmansk@users.noreply.github.com>
2021-09-08 21:37:33 +0530

    Update calfunction3.c

--- implementation/inc/src/calfunction3.c
+++ implementation/inc/src/calfunction3.c
@@ -1,1 +1,10 @@
-
+int multiply()  
+{  
+    int n1, n2, result;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
+    result = n1 * n2;    
+    printf (" The multiply of %d * %d is: %d", n1, n2, result);  
+}   
