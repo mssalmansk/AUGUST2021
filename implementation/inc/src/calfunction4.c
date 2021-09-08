91626f6daf55a4ed397f42cc2ce465f86e4954d8
mssalmansk <47742185+mssalmansk@users.noreply.github.com>
2021-09-08 21:42:12 +0530

    Create calfunction4.c

---
+++ implementation/inc/src/calfunction4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  
+#include <conio.h>  
+#include <math.h>  
+#include <stdlib.h>    
+int addition();  
+int subtract();  
+int multiply();  
+int divide();     
+void exit();
+int divide()  
+{  
+    int n1, n2, result;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
+      
+    if (n2 == 0)  
+    {  
+        printf (" \n Divisor cannot be zero. Please enter another value ");  
+        scanf ("%d", &n2);        
+    }  
+    result = n1 / n2;    
+    printf (" \n The division of %d / %d is: %d", n1, n2, result);  
+}  
